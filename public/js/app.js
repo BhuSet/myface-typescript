c868b2601e14d75bbcedaf00536a049326db8c29
Bhuvi Sethuraman <Bhuvi.Sethuraman@softwire.com>
2021-09-02 20:11:02 +0100

    Part 4 Changing Styles with JS

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Add your JavaScript here.
+
+    const button = document.getElementById("change-color-button");
+    button.addEventListener("click",function(){
+        const postlist = document.getElementsByClassName("box");
+        for(var i = 0;i<postlist.length;i++)
+        {
+            const post = postlist[i];
+            post.style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+
+        }
+    })
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const button = document.getElementById("change-color-button-user-post-page");
+    button.addEventListener("click",function(){
+        const postlist = document.getElementsByClassName("post");
+        for(var i = 0;i<postlist.length;i++)
+        {
+            const post = postlist[i];
+            post.style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+
+        }
+    })
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const button = document.getElementById("change-color-button-user-page");
+    button.addEventListener("click",function(){
+    const body = document.getElementsByTagName("body")[0];
+    body.style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+
+    })
 });
